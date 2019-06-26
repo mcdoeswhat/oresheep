09429397166c1d2cef262b127af8818f91d870ac
mcdoeswhat <2929356483@qq.com>
2019-06-26 19:52:58 +0800

    Add files via upload

---
+++ OreSheep/src/me/albert/oresheep/Commands.java
@@ -0,0 +1,126 @@
+package me.albert.oresheep;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.io.File;
+
+
+public class Commands implements CommandExecutor {
+    private static Main main = Main.getInstance();
+    private Config config = new Config();
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        switch (args.length){
+            case 0:
+                sender.sendMessage(Utils.color("&b/oresheep get <sheep>"));
+                sender.sendMessage(Utils.color("&b/oresheep give <player> <sheep> [amount]"));
+                return true;
+            case 1:
+                if (args[0].equalsIgnoreCase("reload")) {
+                    if (!new File(main.getDataFolder(), "config.yml").exists()) {
+                        main.saveResource("config.yml", true);
+                        sender.sendMessage(config.prefix+config.config_reload);
+                        return true;
+                    } else main.reload();
+                    sender.sendMessage(config.prefix+config.config_reload);
+                    return true;
+                }
+                if (args[0].equalsIgnoreCase("give")) {
+                    sender.sendMessage(Utils.color("&a/oresheep give <player> <sheep> [amount]"));
+                    return true;
+                }
+                if (args[0].equalsIgnoreCase("get")) {
+                    sender.sendMessage(Utils.color("&a/oresheep get <sheep>"));
+                    return true;
+                } else {
+                    sender.sendMessage(config.prefix+config.invalid_usage);
+                    return true;
+                }
+            case 2:
+                if (args[0].equalsIgnoreCase("get") && sender instanceof Player) {
+                    Player p = (Player)sender;
+                    if (main.sheeps.contains(args[1])) {
+                        String name = ChatColor.translateAlternateColorCodes('&',
+                                main.getConfig().getString("Sheep." + args[1] + ".name"));
+                        p.getInventory().addItem(Utils.getEgg(args[1]));
+                        String message = config.egg_give.replace("%amount%","1").replace("%sheep%",name)
+                                .replace("%player%",sender.getName());
+                        p.sendMessage(config.prefix+message);
+                        return true;
+                    } else {
+                        sender.sendMessage(config.prefix+config.sheep_not_found);
+                        return true;
+                    }
+                }
+                if (args[0].equalsIgnoreCase("give")) {
+                    sender.sendMessage(Utils.color("&a/oresheep give <player> <sheep> [amount]"));
+                    return true;
+                }
+                if (args[0].equalsIgnoreCase("get") &&!(sender instanceof Player)){
+                    sender.sendMessage(config.prefix+config.player_only);
+                    return true;
+                }
+                sender.sendMessage(config.prefix+config.invalid_usage);
+                return true;
+            case 3:
+                if (args[0].equalsIgnoreCase("give")) {
+                    if (Bukkit.getPlayer(args[1]) == null) {
+                        sender.sendMessage(config.prefix+config.player_not_found);
+                        return true;
+                    }
+                    Player p = Bukkit.getPlayer(args[1]);
+                    if (main.sheeps.contains(args[2])) {
+                        int amount = 1;
+                        GiveSheep(sender, args, p, amount);
+                        return true;
+                    } else {
+                        sender.sendMessage(config.prefix + config.sheep_not_found);
+                        return true;
+                    }
+
+                }
+            case 4:
+                if (args[0].equalsIgnoreCase("give")) {
+                    if (Bukkit.getPlayer(args[1]) == null) {
+                        sender.sendMessage(config.prefix+config.player_not_found);
+                        return true;
+                    }
+                    Player p = Bukkit.getPlayer(args[1]);
+                    if (main.sheeps.contains(args[2])) {
+                        int amount = Utils.isNumeric(args[3]) ? Integer.parseInt(args[3]): 1;
+                        GiveSheep(sender, args, p, amount);
+                        return true;
+                    } else {
+                        sender.sendMessage(config.prefix + config.sheep_not_found);
+                        return true;
+                    }
+
+                }
+                sender.sendMessage(config.prefix+config.invalid_usage);
+                return true;
+            default:
+                sender.sendMessage(config.prefix+config.invalid_usage);
+                break;
+
+        }
+        return true;
+    }
+
+    private void GiveSheep(CommandSender sender, String[] args, Player p, int amount) {
+        String name =Utils.color(main.getConfig().getString("Sheep." + args[2] + ".name"));
+        ItemStack egg = Utils.getEgg(args[2]);
+        egg.setAmount(amount);
+        p.getInventory().addItem(egg);
+        String message = config.egg_get.replace("%amount%", String.valueOf(amount)).replace("%sheep%", name);
+        String message_sender = config.egg_give.replace("%amount%", String.valueOf(amount)).replace("%sheep%", name)
+                .replace("%player%", p.getName());
+        p.sendMessage(config.prefix + message);
+        sender.sendMessage(config.prefix + message_sender);
+    }
+}
--- OreSheep/src/me/albert/oresheep/Main.java
+++ OreSheep/src/me/albert/oresheep/Main.java
@@ -2,158 +2,51 @@ package me.albert.oresheep;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends JavaPlugin {
     private static Main instance;
-    private static Config config;
+    public static List<String> sheeps = new ArrayList<>();
+    public static List<String> sheepnames = new ArrayList<>();
     CommandSender cs = Bukkit.getConsoleSender();
-    List<String> sheeps = new ArrayList<>();
-    List<String> sheepnames = new ArrayList<>();
-
     @Override
     public void onEnable() {
         cs.sendMessage("§b[OreSheep] Loaded");
         instance = this;
-        this.config = new Config();
         Bukkit.getServer().getPluginManager().registerEvents(new OreSheepListener(), this);
         this.saveDefaultConfig();
         getCommand("oresheep").setTabCompleter(new OreSheepTabCompleter());
+        getCommand("oresheep").setExecutor(new Commands());
         MetricsLite ml = new MetricsLite(this);
-        this.reload();
-    }
-
-    public static Main getInstance() {
-        return instance;
+        reload();
     }
 
     public void reload() {
-        this.reloadConfig();
+        new Config();
+        reloadConfig();
         sheeps.clear();
         sheepnames.clear();
-        for (String key : this.getConfig().getConfigurationSection("Sheep").getKeys(false)) {
+        for (String key : getConfig().getConfigurationSection("Sheep").getKeys(false)) {
             sheeps.add(ChatColor.translateAlternateColorCodes('&', key));
-            sheepnames.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString(
+            sheepnames.add(ChatColor.translateAlternateColorCodes('&', getConfig().getString(
                     "Sheep." + key + ".name")));
         }
 
     }
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        switch (args.length){
-            case 0:
-                sender.sendMessage(Utils.color("&b/oresheep get <sheep>"));
-                sender.sendMessage(Utils.color("&b/oresheep give <player> <sheep> [amount]"));
-                return true;
-            case 1:
-                if (args[0].equalsIgnoreCase("reload")) {
-                    if (!new File(this.getDataFolder(), "config.yml").exists()) {
-                        saveResource("config.yml", true);
-                        sender.sendMessage(config.prefix+config.config_reload);
-                        return true;
-                    } else this.reload();
-                    sender.sendMessage(config.prefix+config.config_reload);
-                    return true;
-                }
-                if (args[0].equalsIgnoreCase("give")) {
-                    sender.sendMessage(Utils.color("&a/oresheep give <player> <sheep> [amount]"));
-                    return true;
-                }
-                if (args[0].equalsIgnoreCase("get")) {
-                    sender.sendMessage(Utils.color("&a/oresheep get <sheep>"));
-                    return true;
-                } else {
-                    sender.sendMessage(config.prefix+config.invalid_usage);
-                    return true;
-                }
-            case 2:
-                if (args[0].equalsIgnoreCase("get") && sender instanceof Player) {
-                    Player p = (Player)sender;
-                    if (sheeps.contains(args[1])) {
-                        String name = ChatColor.translateAlternateColorCodes('&',
-                                this.getConfig().getString("Sheep." + args[1] + ".name"));
-                        p.getInventory().addItem(Utils.getEgg(args[1]));
-                        String message = config.egg_give.replace("%amount%","1").replace("%sheep%",name)
-                                .replace("%player%",sender.getName());
-                        p.sendMessage(config.prefix+message);
-                        return true;
-                    } else {
-                        sender.sendMessage(config.prefix+config.sheep_not_found);
-                        return true;
-                    }
-                }
-                if (args[0].equalsIgnoreCase("give")) {
-                    sender.sendMessage(Utils.color("&a/oresheep give <player> <sheep> [amount]"));
-                    return true;
-                }
-                if (args[0].equalsIgnoreCase("get") &&!(sender instanceof Player)){
-                    sender.sendMessage(config.prefix+config.player_only);
-                    return true;
-                }
-                sender.sendMessage(config.prefix+config.invalid_usage);
-                return true;
-            case 3:
-                if (args[0].equalsIgnoreCase("give")) {
-                    if (Bukkit.getPlayer(args[1]) == null) {
-                        sender.sendMessage(config.prefix+config.player_not_found);
-                        return true;
-                    }
-                    Player p = Bukkit.getPlayer(args[1]);
-                    if (sheeps.contains(args[2])) {
-                        int amount = 1;
-                        GiveSheep(sender, args, p, amount);
-                        return true;
-                    } else {
-                        sender.sendMessage(config.prefix + config.sheep_not_found);
-                        return true;
-                    }
+    public static Main getInstance() {
+        return instance;
+    }
+    @Override
+    public void onDisable() {
+        cs.sendMessage("§c[OreSheep] Disabled..");
 
-                }
-            case 4:
-                if (args[0].equalsIgnoreCase("give")) {
-                    if (Bukkit.getPlayer(args[1]) == null) {
-                        sender.sendMessage(config.prefix+config.player_not_found);
-                        return true;
-                    }
-                    Player p = Bukkit.getPlayer(args[1]);
-                    if (sheeps.contains(args[2])) {
-                        int amount = Utils.isNumeric(args[3]) ? Integer.parseInt(args[3]): 1;
-                        GiveSheep(sender, args, p, amount);
-                        return true;
-                    } else {
-                        sender.sendMessage(config.prefix + config.sheep_not_found);
-                        return true;
-                    }
-
-                }
-                sender.sendMessage(config.prefix+config.invalid_usage);
-                return true;
-                default:
-                    sender.sendMessage(config.prefix+config.invalid_usage);
-                    break;
-
-        }
-        return true;
     }
 
-    private void GiveSheep(CommandSender sender, String[] args, Player p, int amount) {
-        String name =Utils.color(this.getConfig().getString("Sheep." + args[2] + ".name"));
-        ItemStack egg = Utils.getEgg(args[2]);
-        egg.setAmount(amount);
-        p.getInventory().addItem(egg);
-        String message = config.egg_get.replace("%amount%", String.valueOf(amount)).replace("%sheep%", name);
-        String message_sender = config.egg_give.replace("%amount%", String.valueOf(amount)).replace("%sheep%", name)
-                .replace("%player%", p.getName());
-        p.sendMessage(config.prefix + message);
-        sender.sendMessage(config.prefix + message_sender);
-    }
+
 }
 
